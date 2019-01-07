3b90eefd92039caa011ab4c9641b6e706923a91f
Zucker <ph27868@515372-mitll.llan.ll.mit.edu>
2019-01-07 17:05:13 -0500

    thoughts on fan trees and Pum types

--- src/CCC.hs
+++ src/CCC.hs
@@ -33,7 +33,56 @@ instance {-# INCOHERENT #-} (b ~ 'False) => IsTup a b
 
 
 class CCC (flag :: Bool) input out | flag input -> out where -- 
+    toCCC' :: inj -> ext -> input -> out
+class CCC (flag :: Bool) a b out | flag a b -> out where -- 
+    toCCC' :: forall e. (e -> a) -> (b -> e) -> (a -> b) -> out
+
+type family FanCount b where
+    FanCount (x,y) = FanCount x + FanCount y + 1
+    FanCount (x -> y) = FanCount y
+    FanCount Polymorphic = 0
+
+-- We need to build an Either structure with the same depth as the 
+
+-- could we make the base object the can tree tuple type?
+EitherFromFanTree (x,y) = Either (EitherFromFanTree x) (EitherFromFanTree y)
+EitherFromFanTree (x->y) = (EitherFromFanTree y)
+EitherFromFanTree a = a -- otherwise
+
+class injextrFantree index in out were
+    inj :: in  -> out -- EitherFromFanTree b
+    ext :: out -> in
+
+instance InjExtrTree b a o => InjExtrTree b ('Left a) (Either o q)
+   inj x = Left (inj @b @a x)
+   ext x = x' where (Left x') = ext @b @a x
+   -- ext _ = error "Tried to extract left"
+
+
+instance InjExtrTree b ()
+   inj = id 
+   ext = id
+
+-- inj @index. b is global total output type. tells us shape of either tree
+  -- nouse b as an index into the fan tree. Is it unique within the tree?
+
+-- or record path in tree as an either chain 'Left ('Right ())
+-- 
+
+toCCC' :: forall e. (e -> EitherFromFanTree b) -> (EitherFromFanTree b -> e) -> (a -> b) -> out
+
+
+
+class CCC (flag :: Bool) inj ext input out | flag a b -> out inj ext where -- 
+    toCCC' :: inj -> ext -> input -> out
+
+class CCC (flag :: Bool). fanTreePath input out | flag a b -> out inj ext where -- 
     toCCC' :: input -> out
+
+-- inj :: forall e. e -> a
+-- extractor :: forall e. e -> a
+-- f :: a -> b
+-- Really the pair forall e. (e -> a, b -> e  ). This is equivalwent to  b -> a.
 
 -- toCCC reduces to the case of (stuff) -> single thing that is not -> or (,)
 -- curry and fan 
@@ -41,30 +90,55 @@ toCCC :: forall k a b a' b' fb. (
           Category k,
           CCC fb (a -> b) (k a' b'),
           IsTup b fb ) => (a -> b) -> k a' b'
-toCCC f = toCCC' @fb @(a -> b) @(k a' b') f
+toCCC f = toCCC' @fb @(a -> b) @(k a' b') id id f
 
 instance (Cartesian k,
           IsTup b fb,
           IsTup c fc,
-          CCC fb (a -> b) (k a' b'),
-          CCC fc (a -> c) (k a' c')) => CCC 'True (a -> (b,c)) (k a' (b', c')) where
-    toCCC' f = fanC (toCCC' @fb (fst . f)) (toCCC' @fc (snd . f)) 
+          CCC fb   (z -> f (Either x q))  (y -> l)  (a -> b) (k a' b'),
+          CCC fc   (z -> f (Either q' x)) (y -> r) (a -> c) (k a' c')) => CCC 'True  (z -> f x)  (y -> Either l r) (a -> (b,c)) (k a' (b', c')) where
+    toCCC' inj ext f = fanC (toCCC' @fb injL ext1 (fst . f)) (toCCC' @fc injR ext2 (snd . f)) where
+                        injl = inj . Left
+                        injr = inj . Right -- It is a weird lens like structre. Half prism half lens
+                        ext1 = exL . ext
+                        ext2 = exR . ext
+
+
+-- both a product and a sum type. a Pum type
+--newtype Pum a b = PumL a | PumR b
+
+-- newType Pum a b
+-- pumL :: a -> Pum a b
+-- pumL x = unsafeCoerce x
+
+-- PolyCopy. Traverse down structure the get type of a polymorphic copy. New polymorphic variable, but same structure.
+
+-- partial extractors
+-- In a dependently typed language, we could probably instead use a dependent pattern match instead. Output type is a function of the constructor
+exL :: Either a b -> a
+exL (Left x) = x
+exL _ = error "Tried to extract a Left where there is none"
+exR :: Either a b -> b
+exR (Right x) = x
+exR _ = error "Tried to extract a Right where there is none"
+
+
 
 -- curry and then uncurry result
 instance (Closed k,
           IsTup c fc,
           CCC fc ((a,b)->c)  (k (a',b') c')
           ) => CCC 'True (a -> (b -> c)) (k a' (k b' c')) where
-    toCCC' f = curryC (toCCC' @fc (uncurry f)) 
+    toCCC' inj ext f = curryC (toCCC' @fc inj ext (uncurry f)) 
 
 -- base case actually builds the input once the output cannot be detructed more
 -- input can be anything, arrow tuple or polymorphic. Output has to be polymorphic
 instance (Cartesian k,
           IsTup a fa,
-          BuildInput a fa (k a' a'),
-          (k a' b') ~ b) => CCC 'False (a -> b) (k a' b') where
-    toCCC' f = f input where 
-        input = (buildInput @a @fa (idC @k @a'))
+          BuildInput a fa (k a' a'), -- (k a' b') ~ b
+          ) => CCC 'False (  ? -> ? )    (b -> k a' b')    (a -> b) (k a' b') where -- Yeah, no the injector needs to be polymorphic.
+    toCCC' inj ext f = ext (f input) where 
+        input = buildInput @a @fa (\f -> inj (f idC @k @a')) -- Then we need to cps the whole way down. sry.
 
 
 {-
